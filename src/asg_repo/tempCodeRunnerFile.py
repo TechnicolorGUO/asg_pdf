f261f03d71ca24f438241bbca5d446c9ec1c4ae4
22097419d <153608819+22097419d@users.noreply.github.com>
2024-07-31 16:24:11 -0700

    Delete src/asg_repo directory

--- src/asg_repo/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-ors_text = authors_match.group(1).replace('\n', ' ').strip()
-            # cleaned_authors_text = self.clean_authors_text(authors_text)
-            # authors = self.extract_names(cleaned_authors_text)
-            # authors = [re.sub(r'\d+$', '', author).strip() for author in authors]
-            # data['authors'] = ", ".join(authors)
