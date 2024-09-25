8a9fe47774436e5d3c0bde82230cbe63893d0a9b
TechnicolorGUO <guobeichen0228@gmail.com>
2024-09-25 18:43:23 +0800

    update outlien generation

--- src/demo/asg_outline.py
+++ src/demo/asg_outline.py
@@ -2,6 +2,9 @@ import transformers
 import torch
 import pandas as pd
 import os
+import json
+import re
+import ast
 
 class OutlineGenerator():
     def __init__(self, pipeline, df, cluster_names, mode='desp'):
@@ -80,8 +83,8 @@ class OutlineGenerator():
         #     together with the cluster names and the descriptions for entities in each cluster.
         #     '''
         system_prompt = f'''Generate the outline of the survey paper following the format of the example : [[1, '1 Introduction'], [1, '2 Perturbations of (co)differentials'], [2, '2.1 Derivations of the tensor algebra'], [more sections...]].\
-                            The first element in the sub-list refers to the hierachy of the section name (from 1 to 3)\
-                            The second element in the sub-list refers to the section name.
+        The first element in the sub-list refers to the hierachy of the section name (from 1 to 3).\
+        The second element in the sub-list refers to the section name.
         '''
 
         example_json = {"title":"A Survey of Huebschmann and Stasheff's Paper: Formal Solution of the Master Equation via HPT and Deformation Theory","outline":[{"title":"1 Introduction","outline":[]},{"title":"2 Perturbations of (co)differentials","outline":[{"title":"2.1 Derivations of the tensor algebra","outline":[]},{"title":"2.2 Coderivations of the tensor coalgebra","outline":[]},{"title":"2.3 Coderivations of the symmetric coalgebra","outline":[]},{"title":"2.4 DGLA\u2019s and perturbations of the codifferential","outline":[]},{"title":"2.5 Strongly homotopy Lie algebras","outline":[]},{"title":"2.6 The Hochschild chain complex and DGA\u2019s","outline":[]},{"title":"2.7 Strongly homotopy associative algebras","outline":[]}]},{"title":"3 Master equation","outline":[]},{"title":"4 Twisting cochain","outline":[{"title":"4.1 Differential on Hom","outline":[]},{"title":"4.2 Cup product and cup bracket","outline":[]},{"title":"4.3 Twisting cochain","outline":[]}]},{"title":"5 Homological perturbation theory (HPT)","outline":[{"title":"5.1 Contraction","outline":[]},{"title":"5.2 The first main theorem.","outline":[]}]},{"title":"6 Corollaries and the second main theorem","outline":[{"title":"6.1 Other corollaries of Theorem\u00a01.","outline":[]},{"title":"6.2 The second main theorem","outline":[]}]},{"title":"7 Differential Gerstenhaber and BV algebras","outline":[{"title":"7.1 Differential Gerstenhaber algebras","outline":[]},{"title":"7.2 Differential BV algebras","outline":[]},{"title":"7.3 Formality","outline":[{"title":"7.3.1 Formality of differential graded P\ud835\udc43Pitalic_P-algebras","outline":[]},{"title":"7.3.2 Examples","outline":[]}]},{"title":"7.4 Differential BV algebras and formality","outline":[]}]},{"title":"8 Deformation theory","outline":[]},{"title":"References","outline":[]}]}
@@ -91,7 +94,7 @@ class OutlineGenerator():
         messages = [
             {"role": "system", "content": system_prompt}, 
             {"role": "user", "content": user_prompt},
-            {"role": "assistant", "content":"[[1, '1 Introduction'], [1, '2 Abstract'], "}
+            {"role": "assistant", "content":"[[1, '1 Abstract'], [1, '2 Introduction'], "}
             ] 
 
         outputs = self.pipeline(
@@ -103,6 +106,73 @@ class OutlineGenerator():
         self.pipeline.model.disable_adapters()
 
         return messages, result
+
+    
+def parseOutline(survey_id):
+    file_path = f'./src/static/data/txt/{survey_id}.json'
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    prefix = data['messages'][2]['content']   
+    response = data['outline']
+
+    # Extract content between the first '[' and the last ']'
+    def extract_first_last(text):
+        first_match = re.search(r'\[', text)
+        last_match = re.search(r'\](?!.*\])', text)  # Negative lookahead to find the last ']'
+        if first_match and last_match:
+            return text[first_match.start()+1:last_match.start()]
+        return None
+
+    prefix_extracted = extract_first_last(prefix)
+    response_extracted = extract_first_last(response)
+
+    if prefix_extracted:
+        prefix_list = ast.literal_eval(prefix_extracted)
+    else:
+        prefix_list = None
+
+    if response_extracted:
+        outline_list = ast.literal_eval(response_extracted)
+    else:
+        outline_list = None
+
+    outline_list = []
+    for item in prefix_list:
+        outline_list.append(item)
+    for item in outline_list:
+        outline_list.append(item)
+    
+    return outline_list
+
+def generateOutlineHTML(survey_id):
+    prefix_list, outline_list = parseOutline(survey_id)
+    html = '''
+    <div class="outline">
+        <style>
+            .outline {
+                margin: 20px;
+                padding: 10px;
+                border: 1px solid #ccc;
+                border-radius: 5px;
+                background-color: #f9f9f9;
+            }
+            .outline ol {
+                list-style-type: decimal;
+                padding-left: 20px;
+            }
+            .outline li {
+                margin: 5px 0;
+            }
+        </style>
+        <ol>
+    '''
+    for item in outline_list:
+        html += f'<li>{item[1]}</li>'
+    html += '</ol></div>'
+    print(html)
+    print('+++++++++++++++++++++++++++++++++')
+    return html
         
         
 
